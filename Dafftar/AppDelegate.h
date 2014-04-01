37e3f73599459d9d05cb000437fc53706491fc06
apple <mac@apples-Mac-mini.local>
2014-04-01 15:09:51 +0530

    Initial Commit

---
+++ Dafftar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dafftar
+//
+//  Created by apple on 01/04/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dafftar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dafftar
+//
+//  Created by apple on 01/04/14.
+//  Copyright (c) 2014 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
